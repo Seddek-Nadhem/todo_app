afce90b40da6ab254c298e56153ddff77a375c89
Seddek Nadhem <seddeknadhem@gmail.com>
2026-04-21 17:38:25 +0300

    feat: implement TodoRepositoryImpl to bridge Domain and Data layers

---
+++ lib/data/repositories/todo_repository_impl.dart
@@ -0,0 +1,40 @@
+import 'package:todo_app/data/datasources/todo_local_data_source.dart';
+import 'package:todo_app/data/models/todo_model.dart';
+import 'package:todo_app/domain/entities/todo.dart';
+import 'package:todo_app/domain/repositories/todo_repository.dart';
+
+class TodoRepositoryImpl implements TodoRepository {
+  final TodoLocalDataSource localDataSource;
+  TodoRepositoryImpl(this.localDataSource);
+
+  @override
+  Future<List<Todo>> getTodos() async {
+    return await localDataSource.getTodos();
+  }
+
+  @override
+  Future<void> addTodo(Todo todo) async {
+    final model = TodoModel(
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      isCompleted: todo.isCompleted,
+    );
+    await localDataSource.insertTodo(model);
+  }
+
+  @override
+  Future<void> deleteTodo(int id) async {
+    await localDataSource.deleteTodo(id);
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) async {
+    final model = TodoModel(
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      isCompleted: todo.isCompleted,
+    );
+  }
+}
